b0ddf5a4a1bb9616e0ba6a438fc3f7397f55964d
Oluwatimilehin16 <oluwatimilehinpelumi16@gmail.com>
2025-06-08 21:24:27 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+$port = getenv('DB_PORT');
+
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>

f72b977d7a8a4e52d4ed33626b799fab77fff3cd
Oluwatimilehin16 <oluwatimilehinpelumi16@gmail.com>
2025-06-09 14:46:51 +0100

    edits for errors

--- edit_bill_api.php
+++ edit_bill_api.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://rent-tracker-frontend.onrender.com');
 header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
